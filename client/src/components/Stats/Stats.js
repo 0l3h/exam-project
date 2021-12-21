95aa1c21dac9d9ca5fa1cf9b813f80cd39c6b6a1
0l3h <kubik7950@gmail.com>
2021-12-21 14:59:21 +0200

    Modified component layout

--- client/src/components/Stats/Stats.js
+++ client/src/components/Stats/Stats.js
@@ -5,20 +5,20 @@ function Stats () {
   return (
     <div className={styles.statsSection}>
       <div>
+        <img src='staticImages/stars.png' alt='stars' />
         <p>
-          <img src='staticImages/stars.png' alt='stars' />
           <span>4.9 out of 5 stars</span> from 25,000+ customers.
         </p>
       </div>
       <div>
+        <img src='staticImages/people.png' alt='stars' />
         <p>
-          <img src='staticImages/people.png' alt='stars' />
           Our branding community stands <span>200,000+</span> strong.
         </p>
       </div>
       <div>
+        <img src='staticImages/world-globe.png' alt='stars' />
         <p>
-          <img src='staticImages/world-globe.png' alt='stars' />
           <span>140+ Industries</span> supported across more than{' '}
           <span>85 countries</span> – and counting.
         </p>

de66537a07ebf8eb847f1234e3efc3d59629eddb
0l3h <kubik7950@gmail.com>
2021-12-30 20:51:55 +0200

    Added time parsing to Event component

--- client/src/pages/Events/Event/Event.js
+++ client/src/pages/Events/Event/Event.js
@@ -1,18 +1,19 @@
 import React, { useState } from 'react'
+import { formatDuration, intervalToDuration } from 'date-fns'
 import styles from './Event.module.sass'
 
 function Event (props) {
-  const { eventName } = props
-  const remainingTime = Date.now()
+  const { eventName, eventDate } = props
 
-  //   const timerStyles = {
-  //     width:
-  //   }
+  const duration = intervalToDuration({
+    start: Date.now(),
+    end: eventDate
+  })
 
   return (
     <div className={styles.event}>
       <span className={styles.eventName}>{eventName}</span>
-      <span className={styles.remainingTime}>{remainingTime}</span>
+      <span className={styles.remainingTime}>{formatDuration(duration)}</span>
     </div>
   )
 }

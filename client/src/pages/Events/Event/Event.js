38e830c5b6cd323daeb37e3db427fabda7f32e6c
0l3h <kubik7950@gmail.com>
2022-01-06 21:35:39 +0200

    Added timer, dynamic styles

--- client/src/pages/Events/Event/Event.js
+++ client/src/pages/Events/Event/Event.js
@@ -1,17 +1,40 @@
-import React, { useState } from 'react'
-import { formatDuration, intervalToDuration } from 'date-fns'
+import React, { useState, useEffect } from 'react'
+import { addSeconds, formatDuration, intervalToDuration } from 'date-fns'
 import styles from './Event.module.sass'
 
 function Event (props) {
-  const { eventName, eventDate } = props
+  const { id, eventName, eventDate } = props
+
+  const [intervalTime, setIntervalTime] = useState(
+    Number(localStorage.getItem(`intervalTime${id}`))
+  )
+  const [currentTime, setCurrentTime] = useState(Date.now())
+
+  console.log(`intervalTime`, intervalTime)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCurrentTime(new Date(addSeconds(currentTime, 1)))
+    }, 1000)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [currentTime])
+
+  const timerStyles = {
+    width: `${(100 * (intervalTime - (eventDate - currentTime))) /
+      intervalTime}%`
+  }
 
   const duration = intervalToDuration({
-    start: Date.now(),
+    start: currentTime,
     end: eventDate
   })
 
   return (
     <div className={styles.event}>
+      <div className={styles.timerStyles} style={timerStyles}></div>
       <span className={styles.eventName}>{eventName}</span>
       <span className={styles.remainingTime}>{formatDuration(duration)}</span>
     </div>

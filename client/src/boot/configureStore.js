ae5a29f71d8a92666feb74dbf1beabbdc61e89f4
0l3h <kubik7950@gmail.com>
2022-01-24 21:01:05 +0200

    Added loaded initial state to store.

--- client/src/boot/configureStore.js
+++ client/src/boot/configureStore.js
@@ -1,13 +1,24 @@
-import { createStore, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import combineReducers from '../reducers/index';
-import rootSaga from '../sagas/rootSaga';
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import combineReducers from '../reducers/index'
+import rootSaga from '../sagas/rootSaga'
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMiddleware = createSagaMiddleware()
 
-export default function configureStore() {
-  const store = createStore(combineReducers, composeWithDevTools(applyMiddleware(sagaMiddleware)));
-  sagaMiddleware.run(rootSaga, store.dispatch);
-  return store;
+export default function configureStore () {
+  const persistedState = {
+    eventsStore: {
+      events: JSON.parse(localStorage.getItem('events') || '[]'),
+      outdatedEvents: JSON.parse(localStorage.getItem('outdatedEvents') || '[]')
+    }
+  }
+  const store = createStore(
+    combineReducers,
+    persistedState,
+    composeWithDevTools(applyMiddleware(sagaMiddleware))
+  )
+
+  sagaMiddleware.run(rootSaga, store.dispatch)
+  return store
 }

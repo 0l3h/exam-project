049051f2447cff7f6eeac59332ecffc4eabbc475
0l3h <kubik7950@gmail.com>
2022-01-28 21:21:49 +0200

    Added reminder

--- client/src/components/Event/Event.js
+++ client/src/components/Event/Event.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import { addSeconds, formatDuration, intervalToDuration } from 'date-fns'
+import {
+  addSeconds,
+  formatDuration,
+  intervalToDuration,
+  getSeconds
+} from 'date-fns'
 import { toast } from 'react-toastify'
 import PropTypes from 'prop-types'
 import { addOutdatedEvent, deleteEvent } from './../../actions/actionCreator'
@@ -8,9 +13,8 @@ import EventNotification from '../EventNotification/EventNotification'
 import styles from './Event.module.sass'
 
 function Event (props) {
-  const { id, eventName, eventDate, timeAmount } = props
+  const { id, eventName, eventDate, timeAmount, remindTime } = props
   const [currentTime, setCurrentTime] = useState(Date.now())
-  const [hasTimeExpired, setHasTimeExpired] = useState(false)
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -24,18 +28,28 @@ function Event (props) {
   }, [])
 
   useEffect(() => {
-    if (eventDate - currentTime <= 0 && !hasTimeExpired) {
-      setHasTimeExpired(true)
+    console.log(getSeconds(remindTime), getSeconds(eventDate - currentTime))
+    if (eventDate - currentTime <= 0) {
       dispatch(deleteEvent({ id }))
       dispatch(addOutdatedEvent({ id, eventName }))
-      eventAlert()
+      eventOutdatedAlert()
     }
   }, [currentTime])
 
-  const eventAlert = () => {
+  const eventOutdatedAlert = () => {
     toast.error(<EventNotification />, {
       position: toast.POSITION.TOP_CENTER
     })
+  }
+
+  const eventReminderAlert = () => {
+    toast.warn(<div>It's time for {eventName}</div>, {
+      position: toast.POSITION.TOP_CENTER
+    })
+  }
+
+  if (getSeconds(remindTime) === getSeconds(eventDate - currentTime)) {
+    eventReminderAlert()
   }
 
   const timerStyles = {
@@ -60,7 +74,8 @@ Event.propTypes = {
   id: PropTypes.string.isRequired,
   eventName: PropTypes.string.isRequired,
   eventDate: PropTypes.number.isRequired,
-  timeAmount: PropTypes.number.isRequired
+  timeAmount: PropTypes.number.isRequired,
+  remindTime: PropTypes.number.isRequired
 }
 
 export default Event
